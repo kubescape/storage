f308f8eb2b5e8302ec0491b9e59e807fe6bcadf6
Vlad Klokun <vklokun@protonmail.ch>
2023-06-01 17:34:14 +0300

    Revert "Merge pull request #14 from kubescape/evic"
    This reverts commit e05b46de16f8f523d4914805fbed3cbb6af59aa1, reversing
    changes made to 59816c07a504824452939f90df3e2653961cd0d0.
    
    The reverted changes are an implementation of the filesystem-based
    storage. These need to be kept separately before we implement the
    remaining watching function.
    
    Signed-off-by: Vlad Klokun <vklokun@protonmail.ch>

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -17,7 +17,6 @@ limitations under the License.
 package apiserver
 
 import (
-	"github.com/kubescape/storage/pkg/registry/file"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -32,7 +31,6 @@ import (
 	sbomspdxv2p3storage "github.com/kubescape/storage/pkg/registry/softwarecomposition/sbomspdxv2p3"
 	sbomspdxv2p3filteredstorage "github.com/kubescape/storage/pkg/registry/softwarecomposition/sbomspdxv2p3filtered"
 	vmstorage "github.com/kubescape/storage/pkg/registry/softwarecomposition/vulnerabilitymanifest"
-	"github.com/spf13/afero"
 )
 
 const maxRequestBodyBytes = 1024 * 1024 * 1024
@@ -125,11 +123,10 @@ func (c completedConfig) New() (*WardleServer, error) {
 	// https://github.com/kubernetes/kubernetes/issues/86666).
 	apiGroupInfo.NegotiatedSerializer = NewNoProtobufSerializer(Codecs)
 
-	storageImpl := file.NewStorageImpl(afero.NewOsFs(), "/tmp")
 	v1beta1storage := map[string]rest.Storage{}
-	v1beta1storage["sbomspdxv2p3s"] = sbomregistry.RESTInPeace(sbomspdxv2p3storage.NewREST(Scheme, storageImpl, c.GenericConfig.RESTOptionsGetter))
-	v1beta1storage["sbomspdxv2p3filtereds"] = sbomregistry.RESTInPeace(sbomspdxv2p3filteredstorage.NewREST(Scheme, storageImpl, c.GenericConfig.RESTOptionsGetter))
-	v1beta1storage["vulnerabilitymanifests"] = sbomregistry.RESTInPeace(vmstorage.NewREST(Scheme, storageImpl, c.GenericConfig.RESTOptionsGetter))
+	v1beta1storage["sbomspdxv2p3s"] = sbomregistry.RESTInPeace(sbomspdxv2p3storage.NewREST(Scheme, c.GenericConfig.RESTOptionsGetter))
+	v1beta1storage["sbomspdxv2p3filtereds"] = sbomregistry.RESTInPeace(sbomspdxv2p3filteredstorage.NewREST(Scheme, c.GenericConfig.RESTOptionsGetter))
+	v1beta1storage["vulnerabilitymanifests"] = sbomregistry.RESTInPeace(vmstorage.NewREST(Scheme, c.GenericConfig.RESTOptionsGetter))
 
 	apiGroupInfo.VersionedResourcesStorageMap["v1beta1"] = v1beta1storage
 
