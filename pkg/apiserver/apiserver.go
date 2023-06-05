90aaf8a044583ea8965add2dca430d31fcdc85ee
Matthias Bertschy <matthias.bertschy@gmail.com>
2023-06-05 09:40:15 +0200

    move default storage root to file package
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -36,10 +36,7 @@ import (
 	"github.com/spf13/afero"
 )
 
-const (
-	maxRequestBodyBytes = 1024 * 1024 * 1024
-	Root                = "/data"
-)
+const maxRequestBodyBytes = 1024 * 1024 * 1024
 
 var (
 	// Scheme defines methods for serializing and deserializing API objects.
@@ -133,7 +130,7 @@ func (c completedConfig) New() (*WardleServer, error) {
 	// https://github.com/kubernetes/kubernetes/issues/86666).
 	apiGroupInfo.NegotiatedSerializer = NewNoProtobufSerializer(Codecs)
 
-	storageImpl := file.NewStorageImpl(afero.NewOsFs(), Root)
+	storageImpl := file.NewStorageImpl(afero.NewOsFs(), file.DefaultStorageRoot)
 	v1beta1storage := map[string]rest.Storage{}
 	v1beta1storage["sbomspdxv2p3s"] = sbomregistry.RESTInPeace(sbomspdxv2p3storage.NewREST(Scheme, storageImpl, c.GenericConfig.RESTOptionsGetter))
 	v1beta1storage["sbomspdxv2p3filtereds"] = sbomregistry.RESTInPeace(sbomspdxv2p3filteredstorage.NewREST(Scheme, storageImpl, c.GenericConfig.RESTOptionsGetter))
