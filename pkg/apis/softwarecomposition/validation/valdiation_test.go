9940a7cf2c4d51b137300458a9163f1ae6cc1834
Daniel Grunberger <danielgrunberger@armosec.io>
2023-10-24 14:21:41 +0300

    use pointer pkg
    Signed-off-by: Daniel Grunberger <danielgrunberger@armosec.io>

--- pkg/apis/softwarecomposition/validation/valdiation_test.go
+++ pkg/apis/softwarecomposition/validation/valdiation_test.go
@@ -6,11 +6,10 @@ import (
 	"github.com/kubescape/storage/pkg/apis/softwarecomposition"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/utils/pointer"
 )
 
 func TestValidateNetworkNeighbors(t *testing.T) {
-	port80 := int32(80)
-	port1000000 := int32(1000000)
 	tests := []struct {
 		name             string
 		port             int32
@@ -26,7 +25,7 @@ func TestValidateNetworkNeighbors(t *testing.T) {
 							Identifier: "test",
 							Ports: []softwarecomposition.NetworkPort{
 								{
-									Port:     &port80,
+									Port:     pointer.Int32(80),
 									Name:     "UDP-80",
 									Protocol: "UDP",
 								},
@@ -46,7 +45,7 @@ func TestValidateNetworkNeighbors(t *testing.T) {
 							Identifier: "test",
 							Ports: []softwarecomposition.NetworkPort{
 								{
-									Port:     &port80,
+									Port:     pointer.Int32(80),
 									Name:     "UDP",
 									Protocol: "UDP",
 								},
@@ -68,7 +67,7 @@ func TestValidateNetworkNeighbors(t *testing.T) {
 							Identifier: "test",
 							Ports: []softwarecomposition.NetworkPort{
 								{
-									Port:     &port1000000,
+									Port:     pointer.Int32(1000000),
 									Name:     "UDP-1000000",
 									Protocol: "UDP",
 								},
@@ -90,7 +89,7 @@ func TestValidateNetworkNeighbors(t *testing.T) {
 							Identifier: "test",
 							Ports: []softwarecomposition.NetworkPort{
 								{
-									Port:     &port1000000,
+									Port:     pointer.Int32(1000000),
 									Name:     "UDP-80",
 									Protocol: "UDP",
 								},
