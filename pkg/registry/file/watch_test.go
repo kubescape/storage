44acb5647c8fe2fc0c18026df65a5574b24cf294
ttimonen <toni.timonen@iki.fi>
2024-07-22 09:46:33 +0000

    refactor(watcher_test) Simplify the watcher testcase handling
    This is a prequel refactoring that helps to lure out the deadlocks
    in the watcher later.
    
    Signed-off-by: ttimonen <toni.timonen@iki.fi>

--- pkg/registry/file/watch_test.go
+++ pkg/registry/file/watch_test.go
@@ -93,218 +93,64 @@ func TestFileSystemStorageWatchReturnsDistinctWatchers(t *testing.T) {
 	}
 }
 
-func TestFilesystemStoragePublishesToMatchingWatch(t *testing.T) {
+func TestFilesystemStorageWatchPublishing(t *testing.T) {
+	var (
+		keyN = "/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/not-kubescape"
+		keyK = "/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/kubescape"
+		obj  = &v1beta1.SBOMSPDXv2p3{ObjectMeta: v1.ObjectMeta{
+			Name:            "some-sbom",
+			ResourceVersion: "1",
+		}}
+	)
 	tt := []struct {
-		name              string
-		inputWatchesByKey map[string]int
-		inputObjects      map[string]*v1beta1.SBOMSPDXv2p3
-		expectedEvents    map[string][]watch.Event
-	}{
-		{
-			name: "Create should publish to the appropriate single channel",
-			inputWatchesByKey: map[string]int{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/kubescape": 1,
-			},
-			inputObjects: map[string]*v1beta1.SBOMSPDXv2p3{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/kubescape/some-sbom": {
-					ObjectMeta: v1.ObjectMeta{
-						Name: "some-sbom",
-					},
-				},
-			},
-			expectedEvents: map[string][]watch.Event{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/kubescape": {
-					{
-						Type: watch.Added,
-						Object: &v1beta1.SBOMSPDXv2p3{
-							ObjectMeta: v1.ObjectMeta{
-								Name:            "some-sbom",
-								ResourceVersion: "1",
-							},
-						},
-					},
-				},
-			},
+		name                         string
+		start, stopBefore, stopAfter map[string]int
+		inputObjects                 map[string]*v1beta1.SBOMSPDXv2p3
+		want                         map[string][]watch.Event
+	}{{
+		name:  "Create should publish to the appropriate single channel",
+		start: map[string]int{keyK: 1},
+		inputObjects: map[string]*v1beta1.SBOMSPDXv2p3{
+			keyK + "/some-sbom": {ObjectMeta: v1.ObjectMeta{Name: "some-sbom"}},
 		},
-		{
-			name: "Create should publish to all watchers on the relevant key",
-			inputWatchesByKey: map[string]int{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/kubescape": 3,
-			},
-			inputObjects: map[string]*v1beta1.SBOMSPDXv2p3{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/kubescape/some-sbom": {
-					ObjectMeta: v1.ObjectMeta{
-						Name: "some-sbom",
-					},
-				},
-			},
-			expectedEvents: map[string][]watch.Event{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/kubescape": {
-					{
-						Type: watch.Added,
-						Object: &v1beta1.SBOMSPDXv2p3{
-							ObjectMeta: v1.ObjectMeta{
-								Name:            "some-sbom",
-								ResourceVersion: "1",
-							},
-						},
-					},
-					{
-						Type: watch.Added,
-						Object: &v1beta1.SBOMSPDXv2p3{
-							ObjectMeta: v1.ObjectMeta{
-								Name:            "some-sbom",
-								ResourceVersion: "1",
-							},
-						},
-					},
-					{
-						Type: watch.Added,
-						Object: &v1beta1.SBOMSPDXv2p3{
-							ObjectMeta: v1.ObjectMeta{
-								Name:            "some-sbom",
-								ResourceVersion: "1",
-							},
-						},
-					},
-				},
-			},
+		want: map[string][]watch.Event{keyK: {{Type: watch.Added, Object: obj}}},
+	}, {
+		name:  "Create should publish to all watchers on the relevant key",
+		start: map[string]int{keyK: 3},
+		inputObjects: map[string]*v1beta1.SBOMSPDXv2p3{
+			keyK + "/some-sbom": {ObjectMeta: v1.ObjectMeta{Name: "some-sbom"}},
 		},
-		{
-			name: "Creating on key different than the watch should produce no event",
-			inputWatchesByKey: map[string]int{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/kubescape":     3,
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/not-kubescape": 1,
-			},
-			inputObjects: map[string]*v1beta1.SBOMSPDXv2p3{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/not-kubescape/some-sbom": {
-					ObjectMeta: v1.ObjectMeta{
-						Name: "some-sbom",
-					},
-				},
-			},
-			expectedEvents: map[string][]watch.Event{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/not-kubescape": {
-					{
-						Type: watch.Added,
-						Object: &v1beta1.SBOMSPDXv2p3{
-							ObjectMeta: v1.ObjectMeta{
-								Name:            "some-sbom",
-								ResourceVersion: "1",
-							},
-						},
-					},
-				},
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/kubescape": {},
-			},
+		want: map[string][]watch.Event{keyK: {
+			{Type: watch.Added, Object: obj},
+			{Type: watch.Added, Object: obj},
+			{Type: watch.Added, Object: obj},
+		}},
+	}, {
+		name:  "Creating on key different than the watch should produce no event",
+		start: map[string]int{keyK: 3, keyN: 1},
+		inputObjects: map[string]*v1beta1.SBOMSPDXv2p3{
+			keyN + "/some-sbom": {ObjectMeta: v1.ObjectMeta{Name: "some-sbom"}},
 		},
-		{
-			name: "Creating on key not being watched should produce no events",
-			inputWatchesByKey: map[string]int{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/kubescape": 1,
-			},
-			inputObjects: map[string]*v1beta1.SBOMSPDXv2p3{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/not-kubescape/some-sbom": {
-					ObjectMeta: v1.ObjectMeta{
-						Name: "some-sbom",
-					},
-				},
-			},
-			expectedEvents: map[string][]watch.Event{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/not-kubescape": {},
-			},
+		want: map[string][]watch.Event{keyN: {{Type: watch.Added, Object: obj}}, keyK: {}},
+	}, {
+		name:  "Creating on key not being watched should produce no events",
+		start: map[string]int{keyK: 1},
+		inputObjects: map[string]*v1beta1.SBOMSPDXv2p3{
+			keyN + "/some-sbom": {ObjectMeta: v1.ObjectMeta{Name: "some-sbom"}},
 		},
-	}
-
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
-			s := NewStorageImpl(afero.NewMemMapFs(), DefaultStorageRoot)
-			ctx := context.Background()
-			opts := storage.ListOptions{}
-
-			watchSlicesByKey := map[string][]watch.Interface{}
-			for key, watchCount := range tc.inputWatchesByKey {
-				for i := 0; i < watchCount; i++ {
-					watch, _ := s.Watch(ctx, key, opts)
-					currentWatchSlice := watchSlicesByKey[key]
-					currentWatchSlice = append(currentWatchSlice, watch)
-					watchSlicesByKey[key] = currentWatchSlice
-				}
-			}
-
-			var ttl uint64 = 0
-			out := &v1beta1.SBOMSPDXv2p3{}
-			for key, object := range tc.inputObjects {
-				_ = s.Create(ctx, key, object, out, ttl)
-			}
-
-			for key, expectedEvents := range tc.expectedEvents {
-				watches := watchSlicesByKey[key]
-
-				gotEvents := []watch.Event{}
-				for idx := range watches {
-					select {
-					case gotEvent := <-watches[idx].ResultChan():
-						gotEvents = append(gotEvents, gotEvent)
-					case <-time.After(chanWaitTimeout):
-						// Timed out, no event received
-						continue
-					}
-				}
-				assert.Equal(t, expectedEvents, gotEvents)
-			}
-
-		})
-	}
-}
-
-func TestFilesystemStorageWatchStop(t *testing.T) {
-	tt := []struct {
-		name                    string
-		inputWatchesByKey       map[string]int
-		inputStopWatchesAtIndex map[string]int
-		inputObjects            map[string]*v1beta1.SBOMSPDXv2p3
-		expectedEvents          map[string][]watch.Event
-	}{
-		{
-			name: "Sending to stopped watch should not produce an event",
-			inputWatchesByKey: map[string]int{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/not-kubescape": 3,
-			},
-			inputObjects: map[string]*v1beta1.SBOMSPDXv2p3{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/not-kubescape/some-sbom": {
-					ObjectMeta: v1.ObjectMeta{
-						Name: "some-sbom",
-					},
-				},
-			},
-			inputStopWatchesAtIndex: map[string]int{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/not-kubescape": 1,
-			},
-			expectedEvents: map[string][]watch.Event{
-				"/spdx.softwarecomposition.kubescape.io/sbomspdxv2p3s/not-kubescape": {
-					{
-						Type: watch.Added,
-						Object: &v1beta1.SBOMSPDXv2p3{
-							ObjectMeta: v1.ObjectMeta{
-								Name:            "some-sbom",
-								ResourceVersion: "1",
-							},
-						},
-					},
-					{
-						Type: watch.Added,
-						Object: &v1beta1.SBOMSPDXv2p3{
-							ObjectMeta: v1.ObjectMeta{
-								Name:            "some-sbom",
-								ResourceVersion: "1",
-							},
-						},
-					},
-				},
-			},
+		want: map[string][]watch.Event{keyN: {}},
+	}, {
+		name:  "Sending to stopped watch should not produce an event",
+		start: map[string]int{keyN: 3},
+		inputObjects: map[string]*v1beta1.SBOMSPDXv2p3{
+			keyN + "/some-sbom": {ObjectMeta: v1.ObjectMeta{Name: "some-sbom"}},
 		},
-	}
+		stopBefore: map[string]int{keyN: 1},
+		want: map[string][]watch.Event{keyN: {
+			{Type: watch.Added, Object: obj},
+			{Type: watch.Added, Object: obj},
+		}},
+	}}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
@@ -313,48 +159,62 @@ func TestFilesystemStorageWatchStop(t *testing.T) {
 			opts := storage.ListOptions{}
 
 			// Arrange watches
-			watchSlicesByKey := map[string][]watch.Interface{}
-			for key, watchCount := range tc.inputWatchesByKey {
+			watchers := map[string][]watch.Interface{}
+			for key, watchCount := range tc.start {
 				for i := 0; i < watchCount; i++ {
-					watch, _ := s.Watch(ctx, key, opts)
-					currentWatchSlice := watchSlicesByKey[key]
-					currentWatchSlice = append(currentWatchSlice, watch)
-					watchSlicesByKey[key] = currentWatchSlice
+					w, _ := s.Watch(ctx, key, opts)
+					watchers[key] = append(watchers[key], w)
 				}
 			}
 
-			// Arrange stopping of some watches
-			for key, watchIdx := range tc.inputStopWatchesAtIndex {
-				watchSlice := watchSlicesByKey[key]
-				watchSlice[watchIdx].Stop()
-			}
+			// Primitives to stop the watchers gracefully
+			var (
+				done = make(chan bool, 1)
+				wait = func() {
+					select {
+					case <-done:
+					case <-time.After(chanWaitTimeout):
+						t.Errorf("Timed out trying to stop watches")
+					}
+				}
+				stopWatchers = func(ws map[string]int) {
+					for key, i := range ws {
+						watchers[key][i].Stop()
+					}
+					done <- true
+				}
+			)
 
-			// Act out the creation operation
-			var ttl uint64 = 0
-			out := &v1beta1.SBOMSPDXv2p3{}
-			for key, object := range tc.inputObjects {
-				_ = s.Create(ctx, key, object, out, ttl)
+			go stopWatchers(tc.stopBefore)
+			wait()
+			{ // Act out the creation operation
+				var ttl uint64 = 0
+				out := &v1beta1.SBOMSPDXv2p3{}
+				for key, object := range tc.inputObjects {
+					_ = s.Create(ctx, key, object, out, ttl)
+				}
+				time.Sleep(chanWaitTimeout) // Create notifications happen asynchronously
 			}
+			go stopWatchers(tc.stopAfter)
+			wait()
 
 			// Assert the expected events
-			for key, expectedEvents := range tc.expectedEvents {
-				watches := watchSlicesByKey[key]
-
+			for key, wantEvents := range tc.want {
 				gotEvents := []watch.Event{}
-				for idx := range watches {
+				for _, w := range watchers[key] {
 					select {
-					case gotEvent, ok := <-watches[idx].ResultChan():
+					case ev, ok := <-w.ResultChan():
 						// Skip values from closed channels
 						if !ok {
 							continue
 						}
-						gotEvents = append(gotEvents, gotEvent)
+						gotEvents = append(gotEvents, ev)
 					case <-time.After(chanWaitTimeout):
 						// Timed out, no event received
 						continue
 					}
 				}
-				assert.Equal(t, expectedEvents, gotEvents)
+				assert.Equal(t, wantEvents, gotEvents)
 			}
 		})
 	}
@@ -409,13 +269,11 @@ func TestWatchGuaranteedUpdateProducesMatchingEvents(t *testing.T) {
 			s := NewStorageImpl(afero.NewMemMapFs(), DefaultStorageRoot)
 			opts := storage.ListOptions{}
 
-			watchSlicesByKey := map[string][]watch.Interface{}
+			watchers := map[string][]watch.Interface{}
 			for key, watchCount := range tc.inputWatchesByKey {
 				for i := 0; i < watchCount; i++ {
 					watch, _ := s.Watch(context.TODO(), key, opts)
-					currentWatchSlice := watchSlicesByKey[key]
-					currentWatchSlice = append(currentWatchSlice, watch)
-					watchSlicesByKey[key] = currentWatchSlice
+					watchers[key] = append(watchers[key], watch)
 				}
 			}
 
@@ -423,13 +281,11 @@ func TestWatchGuaranteedUpdateProducesMatchingEvents(t *testing.T) {
 			_ = s.GuaranteedUpdate(context.TODO(), tc.args.key, destination, tc.args.ignoreNotFound, tc.args.preconditions, tc.args.tryUpdate, tc.args.cachedExistingObject)
 
 			for key, expectedEvents := range tc.expectedEvents {
-				watches := watchSlicesByKey[key]
-
 				gotEvents := []watch.Event{}
-				for idx := range watches {
+				for _, w := range watchers[key] {
 					select {
-					case gotEvent := <-watches[idx].ResultChan():
-						gotEvents = append(gotEvents, gotEvent)
+					case ev := <-w.ResultChan():
+						gotEvents = append(gotEvents, ev)
 					case <-time.After(chanWaitTimeout):
 						// Timed out, no event received
 						continue
