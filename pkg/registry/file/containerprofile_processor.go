b3384647c01dd800bab8cd56bf17f980a188d19d
shanyl9 <shanyl@armosec.io>
2026-03-13 13:10:25 +0200

    call sendConsolidatedSlugToChannel only for k8s
    Signed-off-by: shanyl9 <shanyl@armosec.io>

--- pkg/registry/file/containerprofile_processor.go
+++ pkg/registry/file/containerprofile_processor.go
@@ -296,8 +296,11 @@ func (a *ContainerProfileProcessor) consolidateKeyTimeSeries(ctx context.Context
 
 	// Send consolidated slug to channel before deleting processed time series
 	// This allows downstream processing even if the ingester dies after consolidation
-	if err := a.sendConsolidatedSlugToChannel(ctx, profile, id); err != nil {
-		return err
+	// Only send for k8s host type
+	if a.HostType == armotypes.HostTypeKubernetes {
+		if err := a.sendConsolidatedSlugToChannel(ctx, profile, id); err != nil {
+			return err
+		}
 	}
 
 	if err := a.deleteProcessedTimeSeries(ctx, processed); err != nil {
