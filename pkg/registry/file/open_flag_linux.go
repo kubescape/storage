4462eed43e1104e6a3cea037167317859d4e7290
Matthias Bertschy <matthias.bertschy@gmail.com>
2026-03-25 17:59:22 +0100

    restore direct I/O flags for payload file operations
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- pkg/registry/file/open_flag_linux.go
+++ pkg/registry/file/open_flag_linux.go
@@ -3,7 +3,10 @@
 
 package file
 
-import "syscall"
+import (
+	"errors"
+	"syscall"
+)
 
 // openFlagDirect contains the platform-specific flag for O_DIRECT on Linux.
 //
@@ -11,3 +14,13 @@ import "syscall"
 // (for example `open_flag_other.go`) with a zero value so that Direct I/O
 // isn't requested when running on macOS or other systems.
 var openFlagDirect = syscall.O_DIRECT
+
+// isDirectIOUnsupported reports whether err indicates that the filesystem does
+// not support O_DIRECT (e.g. tmpfs, overlayfs, some network mounts).
+func isDirectIOUnsupported(err error) bool {
+	var errno syscall.Errno
+	if errors.As(err, &errno) {
+		return errno == syscall.EINVAL || errno == syscall.EOPNOTSUPP
+	}
+	return false
+}
