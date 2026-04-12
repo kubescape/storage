404c8cdd88a54f37c60b2b8ca92a09949c791fe8
Ben <ben@armosec.io>
2026-04-12 13:49:42 +0300

    feat: scaffold SecurityException API group (kubescape.io/v1)
    Add package structure for SecurityException CRD types:
    - Internal and versioned (v1) doc.go with codegen directives
    - Scheme registration for both versions
    - Install package for scheme setup
    
    Types will be added in the next commit.
    Signed-off-by: Ben <ben@armosec.io>

---
+++ pkg/apis/securityexception/doc.go
@@ -0,0 +1,5 @@
+// +k8s:deepcopy-gen=package
+// +groupName=kubescape.io
+
+// Package securityexception is the internal version of the SecurityException API.
+package securityexception
---
+++ pkg/apis/securityexception/v1/register.go
@@ -0,0 +1,39 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// GroupName holds the API group name.
+const GroupName = "kubescape.io"
+
+// SchemeGroupVersion is group version used to register these objects
+var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1"}
+
+var (
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	AddToScheme        = localSchemeBuilder.AddToScheme
+)
+
+func init() {
+	localSchemeBuilder.Register(addKnownTypes)
+}
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&SecurityException{},
+		&SecurityExceptionList{},
+		&ClusterSecurityException{},
+		&ClusterSecurityExceptionList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
